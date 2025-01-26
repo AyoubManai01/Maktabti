f31fd4495f031dcb5e6decd725397c08f0f71e52
Ayoub Manai <92479177+AyoubManai01@users.noreply.github.com>
2025-01-26 17:12:19 +0100

    Add files via upload

---
+++ src/main/java/Entites/Book.java
@@ -0,0 +1,11 @@
+package Entites;
+
+public class Book {
+    public String ISBN;
+    public String Title;
+    public String Subject;
+    public String Publisher;
+    public String Language;
+    public int NumberOfPages;
+
+}
