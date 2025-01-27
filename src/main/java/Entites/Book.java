e12ba0ba29c5ab54883c5d6aab789aacbfbcc012
Ayoub Manai <ayoubmanai000@gmail.com>
2025-01-27 20:33:45 +0100

    Implemented Book, BookItem and Catalog

--- src/main/java/Entites/Book.java
+++ src/main/java/Entites/Book.java
@@ -8,4 +8,62 @@ public class Book {
     public String Language;
     public int NumberOfPages;
 
+    public Book(int numberOfPages, String language, String publisher
+            , String subject, String title, String ISBN) {
+        NumberOfPages = numberOfPages;
+        Language = language;
+        Publisher = publisher;
+        Subject = subject;
+        Title = title;
+        this.ISBN = ISBN;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public String getPublisher() {
+        return Publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        Publisher = publisher;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String language) {
+        Language = language;
+    }
+
+    public int getNumberOfPages() {
+        return NumberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        NumberOfPages = numberOfPages;
+    }
+
 }
---
+++ src/main/java/Entites/Catalog.java
@@ -0,0 +1,72 @@
+package Entites;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class Catalog {
+    public Date creationDate;
+    public int totalBooks;
+    public Map<String, List> bookTitles;
+    public Map<String, List> bookAuthors;
+    public Map<String, List> bookSubjects;
+    public Map<Date, List> bookPublicationDates;
+
+    public Catalog(Map<String, List> bookAuthors, Date creationDate,
+                   int totalBooks, Map<String, List> bookTitles,
+                   Map<String, List> bookSubjects, Map<Date, List> bookPublicationDates) {
+        this.bookAuthors = bookAuthors;
+        this.creationDate = creationDate;
+        this.totalBooks = totalBooks;
+        this.bookTitles = bookTitles;
+        this.bookSubjects = bookSubjects;
+        this.bookPublicationDates = bookPublicationDates;
+    }
+
+    public Map<String, List> getBookSubjects() {
+        return bookSubjects;
+    }
+
+    public void setBookSubjects(Map<String, List> bookSubjects) {
+        this.bookSubjects = bookSubjects;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getTotalBooks() {
+        return totalBooks;
+    }
+
+    public void setTotalBooks(int totalBooks) {
+        this.totalBooks = totalBooks;
+    }
+
+    public Map<String, List> getBookTitles() {
+        return bookTitles;
+    }
+
+    public void setBookTitles(Map<String, List> bookTitles) {
+        this.bookTitles = bookTitles;
+    }
+
+    public Map<String, List> getBookAuthors() {
+        return bookAuthors;
+    }
+
+    public void setBookAuthors(Map<String, List> bookAuthors) {
+        this.bookAuthors = bookAuthors;
+    }
+
+    public Map<Date, List> getBookPublicationDates() {
+        return bookPublicationDates;
+    }
+
+    public void setBookPublicationDates(Map<Date, List> bookPublicationDates) {
+        this.bookPublicationDates = bookPublicationDates;
+    }
+}
