72f439cb45eab28201295f71dbd916dc0929c203
unknown <moenesjaouadi2s2@gmail.com>
2025-01-29 21:04:58 +0100

    adding crud service

--- src/main/java/Services/CreateAccountService.java
+++ src/main/java/Services/CreateAccountService.java
@@ -1,4 +1,22 @@
 package Services;
+import Entites.Account;
+import java.util.List;
 
 public class CreateAccountService {
+    private List<Account> accounts;
+
+    public CreateAccountService(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public boolean createAccount(Account account) {
+        if (accounts.stream().noneMatch(a -> a.getId().equals(account.getId()))) {
+            accounts.add(account);
+            System.out.println("Account created: " + account.getId());
+            return true;
+        }
+        System.out.println("Account creation failed. ID already exists.");
+        return false;
+    }
 }
+
--- src/main/java/Services/ReadAccountService.java
+++ src/main/java/Services/ReadAccountService.java
@@ -1,4 +1,23 @@
 package Services;
 
+
+import Entites.Account;
+
+import java.util.List;
+
 public class ReadAccountService {
+    private List<Account> accounts;
+
+    public ReadAccountService(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Account getAccountById(String id) {
+        return accounts.stream().filter(a -> a.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accounts;
+    }
 }
+
