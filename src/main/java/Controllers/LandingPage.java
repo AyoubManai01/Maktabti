7bdb09005b5e6048494f8197ca06282e193ab0bb
Ayoub Manai <ayoubmanai000@gmail.com>
2025-01-30 20:15:21 +0100

    Created LandingPage GUI

---
+++ src/main/java/Controllers/LandingPage.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LandingPage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/LandingPage.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Library Management System");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
